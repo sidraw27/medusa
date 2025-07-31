9c096ba9f317bd6dbd92aebed8c181b9d01c8906
Sidraw <sidraw.fr@gmail.com>
2025-07-31 14:17:32 +0800

    feat(i18n): add zh-TW language

--- packages/admin/dashboard/src/i18n/config.ts
+++ packages/admin/dashboard/src/i18n/config.ts
@@ -10,7 +10,7 @@ export const defaultI18nOptions: InitOptions = {
     lookupLocalStorage: "lng",
     order: ["cookie", "localStorage", "header"],
   },
-  fallbackLng: "en",
+  fallbackLng: "zhTW",
   interpolation: {
     escapeValue: false,
   },
--- packages/admin/dashboard/src/i18n/types.ts
+++ packages/admin/dashboard/src/i18n/types.ts
@@ -1,8 +1,8 @@
 import type { Locale } from "date-fns"
-import enUS from "./translations/en.json"
+import zhTW from "./translations/zhTW.json"
 
 const resources = {
-  translation: enUS,
+  translation: zhTW,
 } as const
 
 export type Resources = typeof resources
